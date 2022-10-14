4941f1f9bc07ab5a3fb62e997e476b924c5bdd02
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-10-14 20:12:02 +0200

    auth login

--- migrations/20180603120000_seed_matches.js
+++ migrations/20180603120000_seed_matches.js
@@ -119,245 +119,245 @@ exports.up = async (knex) => {
       type: 'ROUND2',
       home_team: 'Danmark 🇩🇰',
       away_team: 'Belgia 🇧🇪',
-      game_start: '2021-06-17 18:00:00+02',
+      game_start: '2023-06-17 18:00:00+02',
       channel: 'TV2'
     },
     {
       type: 'ROUND2',
       home_team: 'Nederland 🇳🇱',
       away_team: 'Østerrike 🇦🇹',
-      game_start: '2021-06-17 21:00:00+02',
+      game_start: '2023-06-17 21:00:00+02',
       channel: 'TV2'
     },
     {
       type: 'ROUND2',
       home_team: 'Sverige 🇸🇪',
       away_team: 'Slovakia 🇸🇰',
-      game_start: '2021-06-18 15:00:00+02',
+      game_start: '2023-06-18 15:00:00+02',
       channel: 'NRK1'
     },
     {
       type: 'ROUND2',
       home_team: 'Kroatia 🇭🇷',
       away_team: 'Tsjekkia 🇨🇿',
-      game_start: '2021-06-18 18:00:00+02',
+      game_start: '2023-06-18 18:00:00+02',
       channel: 'NRK1'
     },
     {
       type: 'ROUND2',
       home_team: 'England 🏴󠁧󠁢󠁥󠁮󠁧󠁿',
       away_team: 'Skottland 🏴󠁧󠁢󠁳󠁣󠁴󠁿',
-      game_start: '2021-06-18 21:00:00+02',
+      game_start: '2023-06-18 21:00:00+02',
       channel: 'TV2'
     },
     {
       type: 'ROUND2',
       home_team: 'Ungarn 🇭🇺',
       away_team: 'Frankrike 🇫🇷',
-      game_start: '2021-06-19 15:00:00+02',
+      game_start: '2023-06-19 15:00:00+02',
       channel: 'TV2'
     },
     {
       type: 'ROUND2',
       home_team: 'Portugal 🇵🇹',
       away_team: 'Tyskland 🇩🇪',
-      game_start: '2021-06-19 18:00:00+02',
+      game_start: '2023-06-19 18:00:00+02',
       channel: 'NRK1'
     },
     {
       type: 'ROUND2',
       home_team: 'Spania 🇪🇸',
       away_team: 'Polen 🇵🇱',
-      game_start: '2021-06-19 21:00:00+02',
+      game_start: '2023-06-19 21:00:00+02',
       channel: 'NRK1'
     },
     {
       type: 'ROUND3',
       home_team: 'Italia 🇮🇹',
       away_team: 'Wales 🏴󠁧󠁢󠁷󠁬󠁳󠁿',
-      game_start: '2021-06-20 18:00:00+02',
+      game_start: '2023-06-20 18:00:00+02',
       channel: 'TV2'
     },
     {
       type: 'ROUND3',
       home_team: 'Sveits 🇨🇭',
       away_team: 'Tyrkia 🇹🇷',
-      game_start: '2021-06-20 18:00:00+02',
+      game_start: '2023-06-20 18:00:00+02',
       channel: 'TV2 SPORT 1'
     },
     {
       type: 'ROUND3',
       home_team: 'Nord-Makedonia 🇲🇰',
       away_team: 'Nederland 🇳🇱',
-      game_start: '2021-06-21 18:00:00+02',
+      game_start: '2023-06-21 18:00:00+02',
       channel: 'TV2'
     },
     {
       type: 'ROUND3',
       home_team: 'Ukraina 🇺🇦',
       away_team: 'Østerrike 🇦🇹',
-      game_start: '2021-06-21 18:00:00+02',
+      game_start: '2023-06-21 18:00:00+02',
       channel: 'TV2 SPORT 1'
     },
     {
       type: 'ROUND3',
       home_team: 'Russland 🇷🇺',
       away_team: 'Danmark 🇩🇰',
-      game_start: '2021-06-21 21:00:00+02',
+      game_start: '2023-06-21 21:00:00+02',
       channel: 'NRK1'
     },
     {
       type: 'ROUND3',
       home_team: 'Finland 🇫🇮',
       away_team: 'Belgia 🇧🇪',
-      game_start: '2021-06-21 21:00:00+02',
+      game_start: '2023-06-21 21:00:00+02',
       channel: 'NRK3'
     },
     {
       type: 'ROUND3',
       home_team: 'Tsjekkia 🇨🇿',
       away_team: 'England 🏴󠁧󠁢󠁥󠁮󠁧󠁿',
-      game_start: '2021-06-22 21:00:00+02',
+      game_start: '2023-06-22 21:00:00+02',
       channel: 'NRK1'
     },
     {
       type: 'ROUND3',
       home_team: 'Kroatia 🇭🇷',
       away_team: 'Skottland 🏴󠁧󠁢󠁳󠁣󠁴󠁿',
-      game_start: '2021-06-22 21:00:00+02',
+      game_start: '2023-06-22 21:00:00+02',
       channel: 'NRK3'
     },
     {
       type: 'ROUND3',
       home_team: 'Sverige󠁮 🇸🇪󠁧󠁿',
       away_team: 'Polen 🇵🇱',
-      game_start: '2021-06-23 18:00:00+02',
+      game_start: '2023-06-23 18:00:00+02',
       channel: 'NRK1'
     },
     {
       type: 'ROUND3',
       home_team: 'Slovakia 🇸🇰',
       away_team: 'Spania 🇪🇸',
-      game_start: '2021-06-23 18:00:00+02',
+      game_start: '2023-06-23 18:00:00+02',
       channel: 'NRK2'
     },
     {
       type: 'ROUND3',
       home_team: 'Portugal 🇵🇹',
       away_team: 'Frankrike 🇫🇷',
-      game_start: '2021-06-23 21:00:00+02',
+      game_start: '2023-06-23 21:00:00+02',
       channel: 'TV2'
     },
     {
       type: 'ROUND3',
       home_team: 'Tyskland 🇩🇪',
       away_team: 'Ungarn 🇭🇺',
-      game_start: '2021-06-23 21:00:00+02',
+      game_start: '2023-06-23 21:00:00+02',
       channel: 'TV2 SPORT 1'
     },
     {
       type: '8',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-06-26 18:00:00+02',
+      game_start: '2023-06-26 18:00:00+02',
       channel: 'NRK1'
     },
     {
       type: '8',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-06-26 21:00:00+02',
+      game_start: '2023-06-26 21:00:00+02',
       channel: 'TV2'
     },
     {
       type: '8',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-06-27 18:00:00+02',
+      game_start: '2023-06-27 18:00:00+02',
       channel: 'TV2'
     },
     {
       type: '8',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-06-27 21:00:00+02',
+      game_start: '2023-06-27 21:00:00+02',
       channel: 'NRK1'
     },
     {
       type: '8',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-06-28 18:00:00+02',
+      game_start: '2023-06-28 18:00:00+02',
       channel: 'NRK1'
     },
     {
       type: '8',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-06-28 21:00:00+02',
+      game_start: '2023-06-28 21:00:00+02',
       channel: 'TV2'
     },
     {
       type: '8',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-06-29 18:00:00+02',
+      game_start: '2023-06-29 18:00:00+02',
       channel: 'TV2'
     },
     {
       type: '8',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-06-29 21:00:00+02',
-      channel: 'NRK1'
-    },
-    {
-      type: 'QUARTER',
-      home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-07-02 18:00:00+02',
+      game_start: '2023-06-29 21:00:00+02',
       channel: 'NRK1'
     },
     {
       type: 'QUARTER',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-07-02 21:00:00+02',
+      game_start: '2023-07-02 18:00:00+02',
+      channel: 'NRK1'
+    },
+    {
+      type: 'QUARTER',
+      home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
+      away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
+      game_start: '2023-07-02 21:00:00+02',
       channel: 'TV2'
     },
     {
       type: 'QUARTER',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-07-03 18:00:00+02',
+      game_start: '2023-07-03 18:00:00+02',
       channel: 'TV2'
     },
     {
       type: 'QUARTER',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-07-03 21:00:00+02',
+      game_start: '2023-07-03 21:00:00+02',
       channel: 'NRK1'
     },
     {
       type: 'SEMI',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-07-06 21:00:00+02',
+      game_start: '2023-07-06 21:00:00+02',
       channel: 'TV2'
     },
     {
       type: 'SEMI',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-07-11 21:00:00+02',
+      game_start: '2023-07-11 21:00:00+02',
       channel: 'TV2'
     },
     {
       type: 'FINAL',
       home_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
       away_team: 'TBD󠁧󠁢󠁥󠁮󠁧󠁿 ',
-      game_start: '2021-07-11 21:00:00+02',
+      game_start: '2023-07-11 21:00:00+02',
       channel: 'NRK1'
     },
   ];
