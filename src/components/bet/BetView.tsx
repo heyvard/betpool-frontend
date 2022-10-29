182ec9df475078f6500c945583c8c0e44da61065
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-10-29 22:52:20 +0200

    invalidate q

--- src/components/bet/BetView.tsx
+++ src/components/bet/BetView.tsx
@@ -1,13 +1,16 @@
-import { Box, Card, CardContent, TextField, Typography } from '@mui/material'
+import { Box, Button, Card, CardContent, TextField, Typography } from '@mui/material'
 import { Bet } from '../../types/types'
 import dayjs from 'dayjs'
 import { useState } from 'react'
+import SaveIcon from '@mui/icons-material/Save'
+import { useQueryClient } from 'react-query'
 
 export const BetView = ({ bet }: { bet: Bet }) => {
     const [hjemmescore, setHjemmescore] = useState<number | undefined>(bet.home_score)
     const [bortescore, setBortescore] = useState<number | undefined>(bet.away_score)
 
     const kampstart = dayjs(bet.game_start)
+    const queryClient = useQueryClient()
 
     const disabled = kampstart.isBefore(dayjs())
     const lagreknapp = hjemmescore !== bet.home_score || bortescore !== bet.away_score
@@ -51,7 +54,17 @@ export const BetView = ({ bet }: { bet: Bet }) => {
                         }}
                     />
                 </Box>
-                {lagreknapp && 'Lagre'}
+                {lagreknapp && (
+                    <Button
+                        variant="contained"
+                        onClick={() => {
+                            queryClient.invalidateQueries(['my-bets']).then()
+                        }}
+                        endIcon={<SaveIcon />}
+                    >
+                        Lagre
+                    </Button>
+                )}
             </CardContent>
         </Card>
     )
