762aac747cf3ac8109d0b2a51bf7b53ca9cf8712
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-05-27 10:21:37 +0200

    180

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -160,6 +160,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <title>Betpool 2024</title>
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+                <link rel="apple-touch-icon" href="/favicon-180x180.png" />
+                <link rel="manifest" href="/manifest.json" />
             </Head>
             <QueryClientProvider client={queryClient}>
                 <UserInnlogging>
