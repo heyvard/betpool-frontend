076b9cd9c5a04e00aeb75394b81bd074c835bb67
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-05-29 19:29:22 +0200

    layout-refaktor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,51 +4,31 @@ import { SignInScreen } from '../components/SignIn'
 import React, { FC, useState } from 'react'
 import { UseUser } from '../queries/useUser'
 import { useRouter } from 'next/router'
-import { Spinner } from '../components/loading/Spinner'
 import Head from 'next/head'
 
 import '../styles/global.css'
 import { getFirebaseAuth } from '../auth/clientApp'
-import { Dropdown, InternalHeader, Loader } from '@navikt/ds-react'
+import { BodyShort, Dropdown, InternalHeader, Loader } from '@navikt/ds-react'
 import { BankNoteIcon, HouseIcon, MenuHamburgerIcon, NumberListIcon, ParagraphIcon } from '@navikt/aksel-icons'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-function UserFetchInnlogging(props: { children: React.ReactNode }) {
-    const { data: me, isLoading } = UseUser()
-    const [user] = useAuthState(getFirebaseAuth())
-
+function Layout({ children }: { children: React.ReactNode }) {
+    const [user, loading] = useAuthState(getFirebaseAuth())
     const router = useRouter()
-
-    if (isLoading || !user || !me) {
-        return <Spinner />
-    }
-
-    const FooterKnapp: FC<{
-        icon: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement> & React.RefAttributes<SVGSVGElement>>
-        text?: string
-        url: string
-        borderRight?: boolean
-    }> = ({ icon: Icon, text, url, borderRight }) => {
-        const router = useRouter()
-        const isActive = router.pathname === url
-
-        return (
-            <InternalHeader.Button
-                className={`flex flex-col items-center w-full ${!text ? 'justify-center' : 'p-2'} ${
-                    borderRight ? 'border-r' : ''
-                } ${isActive ? 'bg-gray-600 text-white' : ''}`}
-                type="button"
-                onClick={() => router.push(url)}
-            >
-                <Icon className="w-8 h-8" />
-                <span className="text-sm">{text}</span>
-            </InternalHeader.Button>
-        )
-    }
+    const { data: me } = UseUser()
 
     return (
         <>
-            <div className="px-2 pt-4 pb-16 mx-auto max-w-full sm:max-w-lg md:max-w-2xl">{props.children}</div>
+            <div className="px-2 pt-4 pb-16 mx-auto max-w-full sm:max-w-lg md:max-w-2xl">
+                {loading && (
+                    <div className="flex justify-center items-center min-h-screen">
+                        <BodyShort>loading</BodyShort>
+                        <Loader size="3xlarge" title="Venter..." />
+                    </div>
+                )}
+                {!loading && !user && <SignInScreen />}
+                {user && <>{children}</>}
+            </div>
             <InternalHeader className="fixed bottom-0 left-0 z-50 w-full h-16 flex ">
                 <FooterKnapp url={'/'} text={''} icon={HouseIcon} />
                 <FooterKnapp text={'Bets'} url={'/my-bets'} icon={BankNoteIcon} />
@@ -56,7 +36,7 @@ function UserFetchInnlogging(props: { children: React.ReactNode }) {
                 <FooterKnapp text={'Regler'} url={'/rules'} icon={ParagraphIcon} />
                 <Dropdown>
                     <InternalHeader.Button
-                        as={Dropdown.Toggle}
+                        as={me ? Dropdown.Toggle : 'button'}
                         name="Velg en underside"
                         className="flex-col items-center w-full justify-center"
                     >
@@ -70,7 +50,7 @@ function UserFetchInnlogging(props: { children: React.ReactNode }) {
                                     router.push('/')
                                 }}
                             >
-                                {user.displayName}
+                                {user?.displayName}
                             </Dropdown.Menu.List.Item>
                             <Dropdown.Menu.List.Item
                                 onClick={() => {
@@ -79,7 +59,7 @@ function UserFetchInnlogging(props: { children: React.ReactNode }) {
                             >
                                 Regler
                             </Dropdown.Menu.List.Item>
-                            {me.scoreadmin && (
+                            {me?.scoreadmin && (
                                 <>
                                     <Dropdown.Menu.List.Item
                                         onClick={() => {
@@ -97,7 +77,7 @@ function UserFetchInnlogging(props: { children: React.ReactNode }) {
                                     </Dropdown.Menu.List.Item>
                                 </>
                             )}
-                            {(me.superadmin || me.paymentadmin) && (
+                            {(me?.superadmin || me?.paymentadmin) && (
                                 <Dropdown.Menu.List.Item
                                     onClick={() => {
                                         router.push('/brukere')
@@ -120,27 +100,6 @@ function UserFetchInnlogging(props: { children: React.ReactNode }) {
             </InternalHeader>
         </>
     )
-}
-
-function UserInnlogging(props: { children: React.ReactNode }) {
-    const [user, loading, error] = useAuthState(getFirebaseAuth())
-    if (loading) {
-        return (
-            <div className="flex justify-center items-center min-h-screen">
-                <Loader size="3xlarge" title="Venter..." />
-            </div>
-        )
-    }
-    if (!user) {
-        return (
-            <>
-                <SignInScreen />
-                {error && <h1>Opps, noe gikk feil {error?.message}</h1>}
-            </>
-        )
-    }
-
-    return <UserFetchInnlogging>{props.children}</UserFetchInnlogging>
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -168,12 +127,36 @@ function MyApp({ Component, pageProps }: AppProps) {
                 <link rel="manifest" href="/manifest.json" />
             </Head>
             <QueryClientProvider client={queryClient}>
-                <UserInnlogging>
+                <Layout>
                     <Component {...pageProps} />
-                </UserInnlogging>
+                </Layout>
             </QueryClientProvider>
         </>
     )
 }
 
+const FooterKnapp: FC<{
+    icon: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement> & React.RefAttributes<SVGSVGElement>>
+    text?: string
+    url: string
+    borderRight?: boolean
+}> = ({ icon: Icon, text, url, borderRight }) => {
+    const router = useRouter()
+    const isActive = router.pathname === url
+    const { data: me } = UseUser()
+    return (
+        <InternalHeader.Button
+            className={`flex flex-col items-center w-full ${!text ? 'justify-center' : 'p-2'} ${
+                borderRight ? 'border-r' : ''
+            } ${isActive && me ? 'bg-gray-600 text-white' : ''}`}
+            type="button"
+            onClick={() => {
+                if (me) return router.push(url)
+            }}
+        >
+            <Icon className="w-8 h-8" />
+            <span className="text-sm">{text}</span>
+        </InternalHeader.Button>
+    )
+}
 export default MyApp
--- src/queries/useUser.ts
+++ src/queries/useUser.ts
@@ -1,15 +1,19 @@
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { getFirebaseAuth } from '../auth/clientApp'
 import { useQuery } from '@tanstack/react-query'
+import { User } from '../types/user'
 
 export function UseUser() {
     const [user] = useAuthState(getFirebaseAuth())
 
-    return useQuery({
+    return useQuery<User>({
         queryKey: ['user-me'],
 
         queryFn: async () => {
             const idtoken = await user?.getIdToken()
+            if (!idtoken) {
+                return undefined
+            }
             let responsePromise = await fetch('/api/v1/me', {
                 method: 'GET',
                 headers: { Authorization: `Bearer ${idtoken}` },
