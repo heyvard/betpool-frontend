da450655f1646098e91e1b79111d9727bd5c2327
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-05-29 19:37:05 +0200

    signinrefaktor

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Container } from '@mui/system'
-import { Card, CardContent, Typography } from '@mui/material'
 import { EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth'
 import StyledFirebaseAuth from '../auth/StyledFirebaseAuth'
+import { BpCard } from './Card'
+import { BodyShort, Heading } from '@navikt/ds-react'
 
 // Configure FirebaseUI.
 const uiConfig = {
@@ -17,47 +17,33 @@ export function SignInScreen() {
         /FB_IAB/.test(navigator.userAgent) || /FBAN/.test(navigator.userAgent) || /FBAV/.test(navigator.userAgent)
     return (
         <>
-            <Container maxWidth="md">
-                <Card sx={{ mt: 1 }}>
-                    <CardContent>
-                        <Typography variant="h4" component="h1" align={'center'}>
-                            VM Betpool
-                        </Typography>
-                    </CardContent>
-                </Card>
+            <BpCard>
+                <Heading size={'large'} align={'center'}>
+                    EM Betpool
+                </Heading>
+            </BpCard>
 
-                <Card sx={{ mt: 1 }}>
-                    <CardContent>
-                        <Typography variant="subtitle2" align={'center'}>
-                            300 kr innskudd 💸
-                        </Typography>
-                    </CardContent>
-                </Card>
-                <Card sx={{ mt: 1 }}>
-                    <CardContent>
-                        <Typography variant="subtitle2" align={'center'}>
-                            Bet frem til kampstart ⚽
-                        </Typography>
-                    </CardContent>
-                </Card>
-                <Card sx={{ mt: 1 }}>
-                    <CardContent>
-                        <Typography variant="subtitle2" align={'center'}>
-                            Hvem kan bli med? Hvem som helst som har denne lenken. 🤝 Bruk din vanlige browser på mobil.
-                        </Typography>
-                    </CardContent>
-                </Card>
-                {isFacebookInAppBrowser && (
-                    <Card sx={{ mt: 1 }}>
-                        <CardContent>
-                            <Typography variant="subtitle2" align={'center'}>
-                                For å logge på må du åpne denne siden utenfor facebook messenger, i vanlig browser.
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                )}
-                {!isFacebookInAppBrowser && <StyledFirebaseAuth uiConfig={uiConfig} />}
-            </Container>
+            <BpCard>
+                <BodyShort align={'center'}>300 kr innskudd 💸</BodyShort>
+            </BpCard>
+            <BpCard>
+                <BodyShort align={'center'}>Bet frem til kampstart på hver kamp, på toppscorer og vinner ⚽</BodyShort>
+            </BpCard>
+            <BpCard>
+                <BodyShort align={'center'}>Hvem kan bli med? Hvem som helst som har denne lenken. 🤝</BodyShort>
+            </BpCard>
+
+            <BpCard>
+                <BodyShort align={'center'}>Bruk din vanlige browser på mobil.</BodyShort>
+            </BpCard>
+            {isFacebookInAppBrowser && (
+                <BpCard>
+                    <BodyShort align={'center'}>
+                        For å logge på må du åpne denne siden utenfor facebook messenger, i vanlig browser.
+                    </BodyShort>
+                </BpCard>
+            )}
+            {!isFacebookInAppBrowser && <StyledFirebaseAuth uiConfig={uiConfig} />}
         </>
     )
 }
