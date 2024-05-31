c21d90b4bba6dde0bfb35732bbfac366ce07b696
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-05-31 09:49:30 +0200

    white

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const BpCard = ({ children }: { children: React.ReactNode | React.ReactNode[] }) => {
-    return <div className={'my-4 p-4 shadow bg-bg-subtle rounded-xl'}>{children}</div>
+    return <div className={'my-4 p-4 shadow bg-white rounded-xl'}>{children}</div>
 }
