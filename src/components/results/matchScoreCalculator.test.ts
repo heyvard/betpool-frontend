ec21ecdf067b5196a025f541cc0f3c84cc280c39
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-06-04 22:35:12 +0200

    tester

--- src/components/results/matchScoreCalculator.test.ts
+++ src/components/results/matchScoreCalculator.test.ts
@@ -1,9 +1,50 @@
 import { regnUtScoreForKamp } from './matchScoreCalculator'
 import { expect } from '@jest/globals'
+import { skapMatchBetArray } from './testdatahelper'
 
 describe('Tester match score calculator', () => {
     it('Tom input', () => {
         const res = regnUtScoreForKamp([])
         expect(res.size).toEqual(0)
+    })
+
+    it('Gruppespill kamp 10 spillere 1 helt rett', () => {
+        const res = regnUtScoreForKamp(
+            skapMatchBetArray({
+                runde: '1',
+                antallHeltRiktig: 1,
+                antallRiktigUtfall: 0,
+                antallFeil: 8,
+            }),
+        )
+        expect(res.size).toEqual(1)
+        expect(res.get('match_id_1')).toEqual({
+            antallRiktigeSvar: 1,
+            antallRiktigeUtfall: 1,
+            matchid: 'match_id_1',
+            riktigResultat: 5,
+            riktigUtfall: 1,
+            utfall: 'B',
+        })
+    })
+
+    it('Finale kamp 10 spillere 1 helt rett', () => {
+        const res = regnUtScoreForKamp(
+            skapMatchBetArray({
+                runde: '7',
+                antallHeltRiktig: 1,
+                antallRiktigUtfall: 0,
+                antallFeil: 8,
+            }),
+        )
+        expect(res.size).toEqual(1)
+        expect(res.get('match_id_1')).toEqual({
+            antallRiktigeSvar: 1,
+            antallRiktigeUtfall: 1,
+            matchid: 'match_id_1',
+            riktigResultat: 15,
+            riktigUtfall: 6,
+            utfall: 'B',
+        })
     })
 })
---
+++ src/components/results/testdatahelper.ts
@@ -0,0 +1,45 @@
+import { MatchBet } from '../../queries/useAllBetsExtended'
+
+export function skapMatchBetArray(opts: {
+    runde: string
+    antallHeltRiktig: number
+    antallRiktigUtfall: number
+    antallFeil: number
+}) {
+    const bets: MatchBet[] = []
+    let user_id = 0
+    const match = {
+        match_id: 'match_id_1',
+        home_result: '1',
+        away_result: '2',
+        round: opts.runde,
+        game_start: 'whatever',
+        home_team: 'Danmkark',
+        away_team: 'Norge',
+    }
+    for (let i = 0; i < opts.antallHeltRiktig; i++) {
+        bets.push({
+            ...match,
+            home_score: '1',
+            away_score: '2',
+            user_id: ++user_id + '',
+        })
+    }
+    for (let i = 0; i < opts.antallRiktigUtfall; i++) {
+        bets.push({
+            ...match,
+            home_score: '0',
+            away_score: '4',
+            user_id: ++user_id + '',
+        })
+    }
+    for (let i = 0; i < opts.antallFeil; i++) {
+        bets.push({
+            ...match,
+            home_score: '0',
+            away_score: '0',
+            user_id: ++user_id + '',
+        })
+    }
+    return bets
+}
