d0525c5099b3c6a52fc4977902bf268eeb95f4bd
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-06-07 21:34:49 +0200

    leaderboard

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,24 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 
-import {
-    Avatar,
-    Box,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Typography,
-} from '@mui/material'
-import { Container } from '@mui/system'
 import { Spinner } from '../components/loading/Spinner'
 import { UseAllBets } from '../queries/useAllBetsExtended'
-import NexyLink from 'next/link'
+import NextLink from 'next/link'
 import { calculateLeaderboard } from '../components/results/calculateAllScores'
-import { Link } from '@navikt/ds-react'
+import { BodyShort, Link, Table } from '@navikt/ds-react'
+import classNames from 'classnames'
 
 function plassVisning(plass: number) {
     switch (plass) {
@@ -42,81 +29,74 @@ const Leaderboard: NextPage = () => {
 
     return (
         <>
-            <Head>
-                <title>Leaderboard</title>
-            </Head>
-            <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
-                <Container maxWidth="md" sx={{ p: 0 }}>
-                    <TableContainer component={Paper}>
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell align="center">Plass</TableCell>
-                                    <TableCell></TableCell>
-                                    <TableCell>Navn</TableCell>
-                                    <TableCell align="right">Poeng</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {lista.map((row, i) => {
-                                    const user = data.users.find((a) => a.id == row.userid)
-                                    if (!user) {
-                                        return null
-                                    }
-                                    return (
-                                        <TableRow
-                                            key={row.userid}
-                                            sx={{
-                                                '&:last-child td, &:last-child th': { border: 0 },
-                                            }}
-                                        >
-                                            <TableCell align="center" sx={{ pt: 1, pb: 2 }}>
-                                                <Typography variant="h2" sx={{ p: 0 }}>
-                                                    {plassVisning(i + 1)}
-                                                </Typography>
-                                            </TableCell>
-                                            <TableCell align="left" sx={{ p: 0 }}>
-                                                {user?.picture && (
-                                                    <NexyLink href={'/user/' + user?.id}>
-                                                        <Avatar
-                                                            alt={user?.name}
-                                                            src={user?.picture}
-                                                            sx={{ width: '50px', height: '50px' }}
-                                                        />
-                                                    </NexyLink>
-                                                )}
-                                                {!user?.picture && (
-                                                    <NexyLink href={'/user/' + user?.id}>
-                                                        <Avatar
-                                                            alt={user?.name}
-                                                            sx={{
-                                                                width: '50px',
-                                                                height: '50px',
-                                                            }}
-                                                        >
-                                                            {user?.name?.substring(0, 1)}
-                                                        </Avatar>
-                                                    </NexyLink>
-                                                )}
-                                            </TableCell>
-                                            <TableCell component="th" scope="row" sx={{ p: 0, pl: 1 }}>
-                                                <NexyLink href={'/user/' + user?.id}>
-                                                    <Link>{user?.name}</Link>
-                                                </NexyLink>
-                                            </TableCell>
-                                            <TableCell align="right" sx={{ p: 0, pr: 3 }}>
-                                                {row.poeng.toFixed(1)}
-                                            </TableCell>
-                                        </TableRow>
-                                    )
-                                })}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Container>
-            </Box>
+            <Table>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell align="center">Plass</Table.HeaderCell>
+                        <Table.HeaderCell></Table.HeaderCell>
+                        <Table.HeaderCell>Navn</Table.HeaderCell>
+                        <Table.HeaderCell align="right">Poeng</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {lista.map((row, i) => {
+                        const user = data.users.find((a) => a.id == row.userid)
+                        if (!user) {
+                            return null
+                        }
+                        return (
+                            <Table.Row key={row.userid}>
+                                <Table.DataCell align="center">
+                                    <BodyShort className={'text-5xl'}>{plassVisning(i + 1)}</BodyShort>
+                                </Table.DataCell>
+                                <Table.DataCell align="left">
+                                    <NextLink href={'/user/' + user?.id}>
+                                        <Avatar src={user?.picture} name={user?.name} />
+                                    </NextLink>
+                                </Table.DataCell>
+                                <Table.DataCell>
+                                    <NextLink href={'/user/' + user?.id}>
+                                        <Link>{user?.name}</Link>
+                                    </NextLink>
+                                </Table.DataCell>
+                                <Table.DataCell align="right">{row.poeng.toFixed(0)}</Table.DataCell>
+                            </Table.Row>
+                        )
+                    })}
+                </Table.Body>
+            </Table>
         </>
     )
 }
 
+interface AvatarProps {
+    src?: string | null
+    name?: string
+    size?: 'small' | 'medium' | 'large'
+}
+
+const Avatar: React.FC<AvatarProps> = ({ src, name, size = 'medium' }) => {
+    const getInitials = (name: string) => {
+        return name.charAt(0).toUpperCase()
+    }
+
+    const sizeClasses = {
+        small: 'w-8 h-8 text-sm',
+        medium: 'w-12 h-12 text-lg',
+        large: 'w-16 h-16 text-xl',
+    }
+
+    return (
+        <div className={classNames('flex items-center justify-center bg-gray-200 rounded-full', sizeClasses[size])}>
+            {src ? (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img src={src} alt={name} className="rounded-full w-full h-full object-cover" />
+            ) : (
+                <span className="text-white bg-blue-500 rounded-full w-full h-full flex items-center justify-center">
+                    {name ? getInitials(name) : ''}
+                </span>
+            )}
+        </div>
+    )
+}
 export default Leaderboard
